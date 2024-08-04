1cb21dbd195ced54dafc0e15db9b7e9e7cf21d25
luca-g <luca-g@users.noreply.github.com>
2024-08-04 15:44:42 +0100

    Add project files.

---
+++ SharedCode/Utils/JwtUtils.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Global
+{
+    public class JwtUtils
+    {
+        private const string DefaultTokenKey = "token";
+        private static byte[]? _key;
+        private static string? _tokenKey;
+        private static double _expireHours = 1;
+        public static string TokenKey => _tokenKey ?? "";
+        public class JwtSettings
+        {
+            public required string SecretKey { get; set; }
+            public string? TokenKey { get; set; }
+            public double? ExpireHours { get; set; }
+        }
+        public static void LoadConfiguration(IConfiguration configuration)
+        {
+            var jwtConfiguration = new JwtSettings() { SecretKey = "" };
+            configuration.GetSection("JwtSettings").Bind(jwtConfiguration);
+            if (string.IsNullOrEmpty(jwtConfiguration?.SecretKey))
+                throw new Exception("JwtConfiguration is not set in appsettings.json");
+            _key = Encoding.ASCII.GetBytes(jwtConfiguration.SecretKey);
+            _tokenKey = jwtConfiguration.TokenKey ?? DefaultTokenKey;
+            _expireHours = jwtConfiguration.ExpireHours ?? _expireHours;
+
+        }
+        public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            LoadConfiguration(configuration);
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateIssuerSigningKey = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(_key),
+                    ValidateLifetime = true
+                };
+
+                // Configure JWT to be read from cookies and headers
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        if(_tokenKey==null)
+                            throw new Exception("Token key _tokenKey is not set");
+                        context.Token = context.Request.Cookies[_tokenKey] ?? context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+        }
+        public static IEnumerable<Claim> GetClaims(string userId, string? userName, string? email)
+        {
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.NameIdentifier, userId));
+            if (userName != null)
+                claims.Add(new Claim(ClaimTypes.Name, userName));
+            if (email != null)
+                claims.Add(new Claim(ClaimTypes.Email, email));
+            return claims.AsEnumerable();
+        }
+        public static string GetTokenString(IEnumerable<Claim>? claims = null)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddHours(_expireHours),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(_key)
+                    , SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return tokenString;
+        }
+        public static void AddTokenToResponseCookie(HttpResponse response, string tokenString)
+        {
+            if (_tokenKey == null)
+                throw new Exception("Token key is not set");
+
+            response.Cookies.Append(_tokenKey, tokenString, new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true, // Ensure this matches the protocol you are using
+                //SameSite = SameSiteMode.Strict,
+                SameSite = SameSiteMode.Strict,
+                Expires = DateTime.UtcNow.AddHours(_expireHours)
+            });
+        }
+    }
+}
---
+++ SharedCode/Utils/SecretsFile.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Configuration;
+namespace Global
+{
+    public class SecretsFile
+    {
+        public static IConfigurationRoot GetConfiguration(string? appSettingsPathName)
+        {
+            try
+            {
+                if (appSettingsPathName == null)
+                {
+                    appSettingsPathName = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+                }
+                if (!File.Exists(appSettingsPathName))
+                {
+                    throw new FileNotFoundException($"app settings file not found {appSettingsPathName}");
+                }
+                return new ConfigurationBuilder()
+                    .SetBasePath(Path.GetDirectoryName(appSettingsPathName)!)
+                    .AddJsonFile(Path.GetFileName(appSettingsPathName))
+                    .Build();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("GetConfiguration error", ex);
+            }
+        }
+        public static string GetSecretFilePathNameFromConfiguration(string? appSettingsPathName, string secretKeyInConfiguration = "secrets-file")
+        {
+            IConfigurationRoot? configuration = null;
+            configuration = GetConfiguration(appSettingsPathName);
+            if (configuration == null)
+            {
+                throw new FileNotFoundException($"configuration file not found on {appSettingsPathName}");
+            }
+            string? secretPathName = null;
+            try
+            {
+                secretPathName = configuration.GetValue<string>(secretKeyInConfiguration);
+                if (string.IsNullOrWhiteSpace(secretPathName))
+                {
+                    throw new MissingFieldException($"configuration file {appSettingsPathName} does not contain secret path name {secretKeyInConfiguration}");
+                }
+                return secretPathName;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"error loading secret configuration file from parent {appSettingsPathName} cofiguration {secretPathName ?? "null"}", ex);
+            }
+        }
+        public static IConfigurationRoot GetSecretConfiguration(string? appSettingsPathName, string secretKeyInConfiguration = "secrets-file")
+        {
+            var secretConfigurationPath = GetSecretFilePathNameFromConfiguration(appSettingsPathName, secretKeyInConfiguration);
+            var configuration = GetConfiguration(secretConfigurationPath);
+            return configuration;
+        }
+    }
+}
